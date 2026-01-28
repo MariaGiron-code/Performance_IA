c954b0c4f334a401905c1fcdfa684c4293419112
Josué Ortiz <168014672+NW08@users.noreply.github.com>
2026-01-28 17:02:49 -0500

    Refactorización del archivo puente entre la API y los recursos

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -1,42 +1,59 @@
+import os
+from typing import Annotated, Generator
+
+from dotenv import load_dotenv
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Session
-import os
-from dotenv import load_dotenv
-from src.database import login  # Reutilizar función de login para autenticación
-from src.logic import ejecutar_prediccion  # Reutilizar función de predicción
+from sqlalchemy.orm import Session, sessionmaker
 
-# Archivo de dependencias para FastAPI: carga de base de datos y autenticación
-# Dedicado a inyección de datos y dependencias comunes
+# Importaciones internas
+from src.database import login
+
+# Se cargan las variables de entorno
 load_dotenv()
 
-DATABASE_URL = os.getenv("DB_URL")
+# Configuración de la BDD
+DB_URL = os.getenv("DB_URL")
 
-# Crear el motor de SQLAlchemy
-engine = create_engine(DATABASE_URL)
+if not DB_URL:
+    raise ValueError("Error Crítico: La variable de entorno 'DB_URL' no está definida.")
 
-# Crear SessionLocal para dependencias
+# Configuración del motor SQL.
+engine = create_engine(DB_URL, pool_pre_ping=True)
+
+# Fábrica de sesiones para las peticiones
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Dependencia para obtener la sesión de base de datos
-def get_db():
+
+# Crea una petición para la BDD
+def get_db() -> Generator[Session, None, None]:
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
 
-# Instancia de HTTPBasic para autenticación
+
+# Define el tipo para insertar dependencias.
+db_dependency = Annotated[Session, Depends(get_db)]
+
+# Seguridad y Autenticación
 security = HTTPBasic()
 
-# Dependencia para obtener el usuario actual autenticado
-def get_current_user(credentials: HTTPBasicCredentials = Depends(security)):
+
+# Valida las credenciales del usuario
+def get_current_user(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
     user = login(credentials.username, credentials.password)
+
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Credenciales inválidas",
+            detail="Credenciales de acceso inválidas",
             headers={"WWW-Authenticate": "Basic"},
         )
-    return user 
+    return user
+
+
+# Define el tipo ingresar al usuario actual
+user_dependency = Annotated[dict, Depends(get_current_user)]
